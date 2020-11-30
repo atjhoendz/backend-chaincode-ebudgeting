586f4747e9bdc04428a27f5504156ed65241787c
atjhoendz <achunarmando@gmail.com>
2020-11-30 21:02:36 +0700

    feat: implement swagger ui

--- src/cars/cars.model.ts
+++ src/cars/cars.model.ts
@@ -1,18 +1,24 @@
 import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CarsDto {
   @IsString()
+  @ApiProperty()
   readonly key: string;
 
   @IsString()
+  @ApiProperty()
   readonly make: string;
 
   @IsString()
+  @ApiProperty()
   readonly model: string;
 
   @IsString()
+  @ApiProperty()
   readonly color: string;
 
   @IsString()
+  @ApiProperty()
   readonly owner: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { EnvConfig } from './common/config/env';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,24 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors();
+
+  /**
+   * Setup Swagger UI
+   */
+  const options = new DocumentBuilder()
+    .setTitle('E-Budgeting Chaincode API')
+    .setDescription('RESTful Chaincode E-Budgeting')
+    .setVersion('1.0')
+    .addTag('chaincode')
+    .setExternalDoc(
+      'Github Repo',
+      'https://github.com/atjhoendz/backend-chaincode-ebudgeting',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(+EnvConfig.PORT, () => {
     Logger.log(`Started Chain-service on PORT ${EnvConfig.PORT}`, 'INFO', true);
