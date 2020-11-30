c7b47cafec980bd175c7315a7956ac8c86a043c2
atjhoendz <achunarmando@gmail.com>
2020-11-30 20:42:42 +0700

    feat: add body parser, validationpipe, enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { EnvConfig } from './common/config/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // json body parser
+  app.use(bodyParser.json());
+
+  // Global request validation
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Enable CORS
+  app.enableCors();
+
+  await app.listen(+EnvConfig.PORT, () => {
+    Logger.log(`Started Chain-service on PORT ${EnvConfig.PORT}`, 'INFO', true);
+  });
 }
 bootstrap();
