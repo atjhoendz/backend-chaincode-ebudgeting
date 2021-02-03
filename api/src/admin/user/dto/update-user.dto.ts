e977a23498b0596e79dbbb81868cf2b726e0176d
atjhoendz <achunarmando@gmail.com>
2021-02-03 23:41:44 +0700

    chore(provinsi): add update dto api prop

--- api/src/admin/user/dto/update-user.dto.ts
+++ api/src/admin/user/dto/update-user.dto.ts
@@ -1,4 +1,42 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export enum Jabatan {
+  Admin = 'Admin',
+  BagianUmum = 'BagianUmum',
+  Pemohon = 'Pemohon',
+  AdminKeuangan = 'AdminKeuangan',
+  KepalaKeuangan = 'KepalaKeuangan',
+  KepalaBiroAUPK = 'KepalaBiroAUPK',
+}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiProperty({
+    description: 'username yang akan digunakan untuk login',
+    example: 'testUsername',
+  })
+  username: string;
+
+  @ApiProperty({
+    description: 'nama lengkap pengguna',
+    example: 'test nama lengkap',
+  })
+  nama_lengkap: string;
+
+  @ApiProperty({
+    description: 'password untuk login',
+    example: '123pwd',
+  })
+  password: string;
+
+  @ApiProperty({
+    example: '192802984278219',
+  })
+  nip: string;
+
+  @ApiProperty({
+    enum: Jabatan,
+    example: 'admin',
+  })
+  jabatan: string;
+}
