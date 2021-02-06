deb6b26ede1efc6b6b95fbcf7cc7b33260fc5221
atjhoendz <achunarmando@gmail.com>
2021-02-06 22:38:58 +0700

    test(user): add some test case

--- api/src/admin/user/user.service.spec.ts
+++ api/src/admin/user/user.service.spec.ts
@@ -2,7 +2,22 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppUtil } from 'src/chaincodeService/appUtil.service';
 import { MockContract } from '../../../test/mockService/mockContract';
 import { HlfConfig } from '../../../test/mockService/mockHlfConfig';
+import { UserDto } from './user.dto';
 import { UserService } from './user.service';
+
+const mockData: UserDto = {
+  docType: 'user',
+  username: 'testUsername',
+  nama_lengkap: 'test nama lengkap',
+  password: '123456',
+  nip: '12341242141211421',
+  jabatan: 'Admin',
+};
+
+const mockState = {
+  Key: expect.any(String),
+  Record: mockData,
+};
 
 describe('UserService', () => {
   let service: UserService;
@@ -20,11 +35,50 @@ describe('UserService', () => {
   });
 
   describe('Find all data', () => {
-    // error on evaluate transaction
     it('should return empty array for empty data', async () => {
       const result = await service.findAll();
 
       expect(result).toEqual('[]');
+    });
+
+    it('should return array of json object if data exist', async () => {
+      await service.create(mockData);
+
+      const result = await service.findAll();
+
+      expect(JSON.parse(result)[0]).toEqual(mockState);
+    });
+  });
+
+  describe('Add a data', () => {
+    it('should return a message if success', async () => {
+      const result = await service.create(mockData);
+
+      expect(result).toEqual('Data berhasil ditambahkan');
+    });
+
+    /*
+     * Todo:
+     * check if props data is not complete
+     */
+  });
+
+  describe('Find spesific data', () => {
+    it('should return object if data is exist', async () => {
+      await service.create(mockData);
+      const resultAll = await service.findAll();
+
+      const key = JSON.parse(resultAll)[0].Key;
+
+      const result = await service.findOne(key);
+
+      expect(JSON.parse(result)).toEqual(mockData);
+    });
+
+    it('should return msg if data is not exist', async () => {
+      const result = await service.findOne(mockState.Key);
+
+      expect(result).toEqual('Data tidak tersedia');
     });
   });
 });
--- api/test/mockService/mockContract.ts
+++ api/test/mockService/mockContract.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { v1 as uuidv1 } from 'uuid';
 
 export enum ListFunc {
   CREATE = 'create',
@@ -8,30 +9,57 @@ export enum ListFunc {
   DELETE = 'deleteByKey',
 }
 
+export type State = {
+  Key: string;
+  Record: Record<string, string>;
+};
+
 @Injectable()
 export class MockContract {
-  private states: Array<any> = [];
+  private states: Array<State> = [];
 
-  // submitTransaction(funcName: string, ...params: string[]) {
-  //   switch (funcName) {
-  //     case ListFunc.CREATE:
-  //       const jsonData = JSON.parse(params[0]);
-  //       this.states.push(jsonData);
-  //       return 'Data berhasil ditambahkan';
-  //     default:
-  //       break;
-  //   }
-  // }
+  submitTransaction = (funcName: string, ...params: string[]) =>
+    new Promise<Buffer>((resolve) => {
+      switch (funcName) {
+        case ListFunc.CREATE:
+          const jsonData = JSON.parse(params[0]);
+
+          const newState: State = {
+            Key: uuidv1(),
+            Record: jsonData,
+          };
+
+          this.states.push(newState);
+
+          let msg: any = Buffer.from('Data tidak berhasil ditambahkan');
+
+          if (this.states.length)
+            msg = Buffer.from('Data berhasil ditambahkan');
+
+          resolve(msg);
+          break;
+      }
+    });
 
   evaluateTransaction = (funcName: string, ...params: string[]) =>
     new Promise<Buffer>((resolve) => {
+      let result;
       switch (funcName) {
         case ListFunc.GETBYTYPE:
-          let result: any = this.states.filter(
-            (state) => state.docType === params[0],
+          result = this.states.filter(
+            (state) => state.Record.docType === params[0],
           );
 
-          if (!result.length) result = '[]';
+          resolve(Buffer.from(JSON.stringify(result)));
+          break;
+        case ListFunc.GETBYKEY:
+          result = this.states.find((state) => state.Key === params[0]);
+
+          if (result) {
+            result = JSON.stringify(result.Record);
+          } else {
+            result = 'Data tidak tersedia';
+          }
 
           resolve(Buffer.from(result));
           break;
