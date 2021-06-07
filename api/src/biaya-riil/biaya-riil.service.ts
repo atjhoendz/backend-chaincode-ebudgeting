3759188d037da67a1431bc8087262aff7b96d763
atjhoendz <achunarmando@gmail.com>
2021-06-07 10:11:52 +0700

    chore(biayaRiilService): handle anggaran on creating new biaya riil

--- api/src/biaya-riil/biaya-riil.service.ts
+++ api/src/biaya-riil/biaya-riil.service.ts
@@ -1,13 +1,64 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { AppUtil } from 'src/chaincode-service/appUtil.service';
 import { HlfConfig } from 'src/chaincode-service/hlfConfig';
 import { BiayaRiilDTO } from './biaya-riil.dto';
+import { AnggaranDTO } from '../anggaran/anggaran.dto';
 
 @Injectable()
 export class BiayaRiilService {
   constructor(private hlfConfig: HlfConfig, private appUtil: AppUtil) {}
 
   async create(biayariilDTO: BiayaRiilDTO) {
+    const queryAnggaran = JSON.stringify({
+      docType: 'anggaran',
+      nama_lembaga: biayariilDTO.nama_lembaga,
+    });
+
+    const dataAnggaranAsBuffer = await this.hlfConfig.contract.evaluateTransaction(
+      'getByQuery',
+      queryAnggaran,
+    );
+
+    const dataAnggaranAsString = this.appUtil.prettyJSONString(
+      dataAnggaranAsBuffer,
+    );
+
+    let dataAnggaranAsJSON = JSON.parse(dataAnggaranAsString);
+
+    if (!dataAnggaranAsJSON.length)
+      throw new NotFoundException(
+        `Nama lembaga is not found, no record ${biayariilDTO.nama_lembaga} on anggaran docType`,
+      );
+
+    dataAnggaranAsJSON = dataAnggaranAsJSON[0];
+
+    if (biayariilDTO.total > dataAnggaranAsJSON.Record.sisa_anggaran)
+      throw new BadRequestException({
+        statusCode: 400,
+        type: 'info',
+        msg: 'Total biaya riil melebihi sisa alokasi anggaran.',
+      });
+
+    const sisaAnggaran =
+      parseInt(dataAnggaranAsJSON.Record.sisa_anggaran) -
+      parseInt(biayariilDTO.total);
+
+    const updateDataAnggaran: AnggaranDTO = {
+      docType: dataAnggaranAsJSON.Record.docType,
+      nama_lembaga: dataAnggaranAsJSON.Record.nama_lembaga,
+      sisa_anggaran: sisaAnggaran,
+    };
+
+    await this.hlfConfig.contract.submitTransaction(
+      'updateByKey',
+      dataAnggaranAsJSON.Key,
+      JSON.stringify(updateDataAnggaran),
+    );
+
     const result = await this.hlfConfig.contract.submitTransaction(
       'create',
       JSON.stringify(biayariilDTO),
@@ -31,6 +82,24 @@ export class BiayaRiilService {
     const result = await this.hlfConfig.contract.evaluateTransaction(
       'getByKey',
       key,
+    );
+
+    return this.appUtil.prettyJSONString(result);
+  }
+
+  async findByQuery(key: string, value: string) {
+    if (!key || !value)
+      throw new BadRequestException(
+        'Key or value argument are cannot be empty',
+      );
+
+    const query = JSON.stringify({
+      [key]: value,
+    });
+
+    const result = await this.hlfConfig.contract.evaluateTransaction(
+      'getByQuery',
+      query,
     );
 
     return this.appUtil.prettyJSONString(result);
