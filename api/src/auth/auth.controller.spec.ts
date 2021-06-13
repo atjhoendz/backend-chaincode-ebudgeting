924b904f070a4c13ec63856d81b1088269c06ce5
atjhoendz <achunarmando@gmail.com>
2021-06-13 23:08:50 +0700

    test(AuthController): :white_check_mark: add module definition for test

--- api/src/auth/auth.controller.spec.ts
+++ api/src/auth/auth.controller.spec.ts
@@ -1,12 +1,38 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { Test, TestingModule } from '@nestjs/testing';
+import configuration from 'src/config/configuration';
+import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        UserModule,
+        PassportModule,
+        ConfigModule.forRoot({
+          isGlobal: true,
+          load: [configuration],
+        }),
+        JwtModule.registerAsync({
+          useFactory: async (configService: ConfigService) => ({
+            secret: configService.get<string>('JWT_ACCESS_TOKEN_SECRET'),
+            signOptions: {
+              expiresIn: configService.get<string>(
+                'JWT_ACCESS_TOKEN_EXPIRES_TIME',
+              ),
+            },
+          }),
+          inject: [ConfigService],
+        }),
+      ],
       controllers: [AuthController],
+      providers: [AuthService],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
